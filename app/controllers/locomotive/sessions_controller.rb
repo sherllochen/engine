489a2206a2ca3254a60c2305ba822ee369531a44
neversion <neversion@neversiondeMacBook-Air.local>
2016-08-27 12:16:13 +0800

    specific path for normal user after login

--- app/controllers/locomotive/sessions_controller.rb
+++ app/controllers/locomotive/sessions_controller.rb
@@ -16,7 +16,15 @@ module Locomotive
     private
 
     def after_sign_in_path_for(resource)
-      current_site? ? dashboard_path(current_site) : sites_path
+      cn_site = resource.sites.first
+      if cn_site.memberships.where(account_id:resource.id).first.normal?
+        content_type = cn_site.content_types.where(slug: 'faculities').first
+        own_entry = content_type.entries.where(email:current_locomotive_account.email).first
+        edit_content_entry_path(cn_site, content_type.slug, own_entry)
+      else
+        current_site? ? dashboard_path(current_site) : sites_path
+      end
+
     end
 
     def after_sign_out_path_for(resource)
